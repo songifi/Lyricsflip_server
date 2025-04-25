f8dd5b90622896b70b12ff2855e0bc0620de5e32
AdamsYab <adamsyabaya@gmail.com>
2025-04-25 12:07:37 +0100

    Implement Full User Module with CRUD and Role Management

---
+++ src/users/entities/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, IsEnum, IsOptional } from 'class-validator';
+import { Role } from '../enums/role.enum';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsEnum(Role)
+  @IsOptional()
+  role?: Role;
+}
